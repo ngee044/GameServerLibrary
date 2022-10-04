8bc5aafa9bf4c5fed8937834a60900733ba493ad
LeeHyunKyu <38107616+ngee044@users.noreply.github.com>
2022-10-05 00:30:29 +0900

    논블로킹 사용법
    소켓프로그래밍

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -17,7 +17,7 @@ void error_message()
 
 int main()
 {
-	//왼쪽 초기화 (ws2_32 라이브러리 초기화)
+#if 0	//왼쪽 초기화 (ws2_32 라이브러리 초기화)
 	//관련 정보 wsa_data 채워짐
 	WSAData wsa_data;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0)
@@ -80,4 +80,164 @@ int main()
 	::WSACleanup();
 
 	return 0;
+#elif 0
+	WSAData wsa_data;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0)
+	{
+		std::cout << __func__ << __LINE__ << std::endl;
+		return 0;
+	}
+
+	SOCKET client_socket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	if (client_socket == INVALID_SOCKET)
+	{
+		std::cout << __func__ << __LINE__ << std::endl;
+		return 0;
+	}
+
+	SOCKADDR_IN server_addr;
+	::memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+	server_addr.sin_port = ::htons(7777); //host to network short // 80 : HTTP
+
+
+	while (true)
+	{
+		//TODO.......
+		char send_buffer[100] = "hello world!!";
+		auto result = ::sendto(client_socket, send_buffer, sizeof(send_buffer), 0, (SOCKADDR*)&server_addr, sizeof(server_addr));
+		if (result == SOCKET_ERROR)
+		{
+			error_message();
+			return 0;
+		}
+
+		std::cout << "send data len = " << sizeof(send_buffer) << endl;
+
+		SOCKADDR_IN recv_addr;
+		::memset(&recv_addr, 0, sizeof(recv_addr));
+		int32 addr_len = sizeof(recv_addr);
+
+		char recv_buffer[1000];
+
+		int32 recv_len = ::recvfrom(client_socket, recv_buffer, sizeof(recv_buffer), 0, (SOCKADDR*)&recv_addr, &addr_len);
+
+		if (recv_len <= 0)
+		{
+			error_message();
+			return 0;
+		}
+
+		std::cout << "Recv Data! data = " << recv_buffer << std::endl;
+		std::cout << "Recv Data! len = " << recv_len << std::endl;
+
+		std::this_thread::sleep_for(1s);
+	}
+
+	closesocket(client_socket);
+
+	::WSACleanup();
+
+	return 0;
+#else
+	WSAData wsa_data;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0)
+	{
+		return 0;
+	}
+
+	SOCKET client_socket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (client_socket == INVALID_SOCKET)
+	{
+		return 0;
+	}
+
+	u_long on = 1;
+	if (::ioctlsocket(client_socket, FIONBIO, &on) == INVALID_SOCKET)
+	{
+		return 0;
+	}
+
+	SOCKADDR_IN server_addr;
+	::memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+	server_addr.sin_port = ::htons(7777);
+
+	//connect
+	while (true)
+	{
+		if (::connect(client_socket, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+		{
+			//do nonblocking...
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+			{
+				continue;
+			}
+			
+			//already connected is break
+			if (::WSAGetLastError() == WSAEISCONN)
+			{
+				break;
+			}
+			//error
+			break;
+		}
+	}
+
+	std::cout << "Connected to server!" << std::endl;
+
+	char send_buffer[100] = "hello world";
+
+	//send
+	while (true)
+	{
+		if (::send(client_socket, send_buffer, sizeof(send_buffer), 0) == SOCKET_ERROR)
+		{
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+			{
+				continue;
+			}
+			//Error
+			break;
+		}
+		std::cout << "Send Data ! Len = " << sizeof(send_buffer) << std::endl;
+
+		//recv
+		while (true)
+		{
+			char recv_buffer[1000];
+			int32 recv_len = ::recv(client_socket, recv_buffer, sizeof(recv_buffer), 0);
+			if (recv_len == SOCKET_ERROR)
+			{
+				//do nonblocking
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				{
+					continue;
+				}
+				//Error
+				break;
+			}
+			else if (recv_len == 0)
+			{
+				//disconnect (연결끊김)
+				break;
+			}
+
+			std::cout << "Recv Data Len = " << recv_len << std::endl;
+
+			break;
+		}
+
+		std::this_thread::sleep_for(1s);
+	}
+
+	//socket resource return
+	::closesocket(client_socket);
+
+	//winsock exit
+	::WSACleanup();
+
+#endif
 }
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -22,6 +22,7 @@ void error_message()
 
 int main()
 {
+#if 0
 	//왼쪽 초기화 (ws2_32 라이브러리 초기화)
 	//관련 정보 wsa_data 채워짐
 	WSAData wsa_data;
@@ -104,5 +105,170 @@ int main()
 
 	::WSACleanup();
 	return 0;
+#elif 0
+	WSAData wsa_data;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0)
+	{
+		return 0;
+	}
+
+	SOCKET server_socket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	if (server_socket == INVALID_SOCKET)
+	{
+		std::cout << __func__ << __LINE__ << std::endl;
+		return 0;
+	}
+
+	SOCKADDR_IN server_addr;
+	::memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+	server_addr.sin_port = ::htons(7777);
+
+	if (::bind(server_socket, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+	{
+		std::cout << __func__ << __LINE__ << std::endl;
+		return 0;
+	}
+
+	while (true)
+	{
+		SOCKADDR_IN client_addr;
+		::memset(&client_addr, 0, sizeof(client_addr));
+		int32 addr_len = sizeof(client_addr);
+
+		char recv_buffer[1000];
+		
+		int32 recv_len = ::recvfrom(server_socket, recv_buffer, sizeof(recv_buffer), 0, (SOCKADDR*)&client_addr, &addr_len);
+		if (recv_len <= 0)
+		{
+			return 0;
+		}
+
+		std::cout << "Recv Data! data = " << recv_buffer << std::endl;
+		std::cout << "Recv Data! len = " << recv_len << std::endl;
+
+		int32 error_code = ::sendto(server_socket, recv_buffer, recv_len, 0, (SOCKADDR*)&client_addr, sizeof(client_addr));
+		
+		if (error_code == SOCKET_ERROR)
+		{
+			std::cout << __func__ << __LINE__ << std::endl;
+			return 0;
+		}
+		std::cout << "Send Data! Len = " << recv_len << std::endl;
+	}
+
+	::WSACleanup();
+	return 0;
+#else
+	WSAData wsa_data;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0)
+	{
+		return 0;
+	}
+
+	//블로킹 소켓
+	//accecpt -> 접속한 클라가 있을 때
+	//connect -> 접속 성공했을 때
+	//send, sendto -> 요청한 데이터를 송신 버퍼에 복사했을 때
+	//recv, recvfrom -> 수신 버퍼에 도착한 데이터가 있고, 이를 유저레벨 버퍼에 복사했을 때
+	
+	//논블로킹(Non-Blocking)
+
+	SOCKET listen_socket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_socket == INVALID_SOCKET)
+	{
+		return 0;
+	}
+
+	u_long on = 1;
+	if (::ioctlsocket(listen_socket, FIONBIO, &on) == INVALID_SOCKET)
+	{
+		return 0;
+	}
+
+	SOCKADDR_IN server_addr;
+	::memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+	server_addr.sin_port = ::htons(7777);
+
+	if (::bind(listen_socket, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)
+	{
+		return 0;
+	}
+
+	if (::listen(listen_socket, SOMAXCONN) == SOCKET_ERROR)
+	{
+		return 0;
+	}
+
+	std::cout << "Accept" << endl;
+
+	SOCKADDR_IN client_addr;
+	int32 addr_len = sizeof(client_addr);
+
+	while (true)
+	{
+		SOCKET client_socket = ::accept(listen_socket, (SOCKADDR*)&client_addr, &addr_len);
+		if (client_socket == INVALID_SOCKET)
+		{
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+			{
+				continue;
+			}
+			//Error
+			break;
+		}
+
+		std::cout << "client connected!" << std::endl;
+
+		//Recv
+		while (true)
+		{
+			char recv_buffer[1000];
+			int32 recv_len = ::recv(client_socket, recv_buffer, sizeof(recv_buffer), 0);
+			if (recv_len == SOCKET_ERROR)
+			{
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				{
+					continue;
+				}
+				//Error
+				break;
+			}
+			else if (recv_len == 0)
+			{
+				//disconnect (연결끊김)
+				break;
+			}
+
+			std::cout << "Recv Data Len = " << recv_len << std::endl;
+
+			//send
+			while (true)
+			{
+				if (::send(client_socket, recv_buffer, recv_len, 0) == SOCKET_ERROR)
+				{
+					if (::WSAGetLastError() == WSAEWOULDBLOCK)
+					{
+						continue;
+					}
+					//Error
+					break;
+				}
+
+				std::cout << "Send Data ! Len = " << recv_len << std::endl;
+				break;
+			}
+		}
+	}
+
+	::WSACleanup();
+
+#endif
 }
+
+
+
 
